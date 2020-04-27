aadbda7fb7dbe4cfaf7cd4e7586e90b8b146d1f3
bstalport <50781601+bstalport@users.noreply.github.com>
2020-04-27 09:33:51 +0200

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,14 +9,14 @@ class Fbase {
   static initialize() {
     try {
       return firebase.initializeApp({
-        apiKey: 'AIzaSyCLGdh96dQwTwT84hOA3dMrioFcTNr_JiY',
-        authDomain: 'run4them-1b0cf.firebaseapp.com',
-        databaseURL: 'https://run4them-1b0cf.firebaseio.com',
-        projectId: 'run4them-1b0cf',
-        storageBucket: 'run4them-1b0cf.appspot.com',
-        messagingSenderId: '456194470833',
-        appId: '1:456194470833:web:e3154985546f0d79524390',
-        measurementId: 'G-QY59XJ3V57',
+        apiKey: '',
+        authDomain: '',
+        databaseURL: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: '',
+        appId: '',
+        measurementId: '',
       });
     } catch (err) {
       // we skip the "already exists" message which is
